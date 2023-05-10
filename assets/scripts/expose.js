1c78d9f714482299e565c2d48545399c43fbd7da
Doheon Kim <peterjhms1922@gmail.com>
2023-05-10 05:01:24 -0700

    added

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,76 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  //const exposed = document.getElementById("expose");
+  const hornchange = document.getElementById("horn-select");
+  const volume_control = document.getElementById("volume-controls");
+  const volume = document.getElementById("volume");
+  const audio = document.querySelector("audio");
+  const button = document.querySelector("button");
+  const imgs = document.querySelector("img");
+  const volimgs = volume_control.querySelector("img");
+  
+  hornchange.addEventListener('change',function() {
+    changehorn(hornchange,audio,imgs);
+    });
+  button.addEventListener('click', () => {
+    audio.play();
+    const buttons = button.value;
+    if (hornchange.value ==  'party-horn'){
+      const jsconfetti = new JSConfetti();
+      jsconfetti.addConfetti();
+
+    }
+
+
+  });
+  volume.addEventListener('input',function(){
+    changeinvolume(volume,audio,volimgs);
+  });
+
+
 }
+function changeinvolume(volume,audio,volimgs){
+  const volumes = volume.value;
+  audio.volume = volumes/100;
+
+
+  if (volumes == '0') {
+    volimgs.src = "assets/icons/volume-level-0.svg";
+    volimgs.alt = 'Volume level 0';
+  }
+  else if (volumes < '33') {
+    volimgs.src = "assets/icons/volume-level-1.svg";
+  }
+  else if (volumes < '67') {
+    volimgs.src = "assets/icons/volume-level-2.svg";
+  }
+  else {
+    volimgs.src = "assets/icons/volume-level-3.svg";
+  }
+}
+function changehorn(hornchange,audio,imgs){
+  if (hornchange.value == "air-horn"){
+        audio.src = "/assets/audio/air-horn.mp3";
+        imgs.src = "/assets/images/air-horn.svg";
+      
+  }
+  else if (hornchange.value == "car-horn"){
+       
+        imgs.src = "/assets/images/car-horn.svg";
+        audio.src = "/assets/audio/car-horn.mp3";
+  
+  }
+  else if (hornchange.value =="party-horn"){
+        imgs.src = "/assets/images/party-horn.svg";
+        audio.src = "/assets/audio/party-horn.mp3";
+ 
+  }
+}
+
+
+
+//hornchange.addEventListener("change",changehorn);
+
+
+ 
