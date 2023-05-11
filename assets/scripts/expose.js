39f8047d270f29184e9eb1d84b1d42da7e55ef8a
Doheon Kim <peterjhms1922@gmail.com>
2023-05-11 16:23:13 -0700

    update

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -14,16 +14,16 @@ function init() {
   const volimgs = volume_control.querySelector("img");
   
   hornchange.addEventListener('change',() => {
-    if (hornchange.value === "air-horn"){
+    if (hornchange.value == "air-horn"){
       audio.src = "./assets/audio/air-horn.mp3";
       imgs.src = "./assets/images/air-horn.svg";
     }
-    else if (hornchange.value === "car-horn"){
+    else if (hornchange.value == "car-horn"){
      
       imgs.src = "./assets/images/car-horn.svg";
       audio.src = "./assets/audio/car-horn.mp3";
     }
-    else if (hornchange.value === "party-horn"){
+    else if (hornchange.value == "party-horn"){
       imgs.src = "./assets/images/party-horn.svg";
       audio.src = "./assets/audio/party-horn.mp3";
     };
